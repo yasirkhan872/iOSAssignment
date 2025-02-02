5e2837628bc2cccc09c5b4858d2d90c0e9cb26ae
Yasir Khan <yasirkhan872@gmail.com>
2025-02-02 09:52:31 +0400

    Initial Commit

---
+++ iOSAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSAssignment
+//
+//  Created by Yasir Khan on 02/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
